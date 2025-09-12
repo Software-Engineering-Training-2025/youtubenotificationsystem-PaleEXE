6c402b1ddfd0a1c4b99c556015fc1b9f6235e891
PaleEXE <150634604+PaleEXE@users.noreply.github.com>
2025-09-12 04:32:14 +0300

    task4: implement channelService

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,46 @@
 package org.example;
 
+import org.example.observer.User;
+import org.example.subject.Channel;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO:
-        // 1. Create some Channels
-        // 2. Create some Users
-        // 3. Subscribe Users to Channels
-        // 4. Upload videos and see notifications propagate
+        ChannelService channelService = new ChannelService();
+
+        User pale = new User("Mohammad");
+        User fadi = new User("Fadi");
+        User mimi = new User("Mimi");
+        User silk = new User("Hornet");
+
+        // I did not check for empty optional because it is a test code
+        // of course I would add proper handling in the real API
+        Channel paleChannel = channelService.createNewChannel(pale, "Pale").get();
+        Channel mimiChannel = channelService.createNewChannel(mimi, "MIMI").get();
+        Channel fadiChannel = channelService.createNewChannel(fadi, "EXE_").get();
+        Channel silkChannel = channelService.createNewChannel(silk, "SHAW").get();
+
+        channelService.subscribeToChannel(paleChannel, mimiChannel);
+        channelService.subscribeToChannel(fadiChannel, mimiChannel);
+        channelService.subscribeToChannel(silkChannel, mimiChannel);
+
+        channelService.subscribeToChannel(mimiChannel, silkChannel);
+        channelService.subscribeToChannel(paleChannel, silkChannel);
+
+        channelService.subscribeToChannel(fadiChannel, paleChannel);
+        channelService.subscribeToChannel(mimiChannel, paleChannel);
+
+        mimiChannel.uploadVideo("Hollow Knight PeakSong iz ez");
+        silkChannel.uploadVideo("GIT GUD");
+        fadiChannel.uploadVideo("IDK");
+
+        // test duplicate channel names
+        Channel _noMimiChannel = channelService.createNewChannel(mimi, "MIMI").orElse(null);
+
+        // test subscribe twice
+        channelService.subscribeToChannel(silkChannel, mimiChannel);
+
+        // test unsubscribe
+        mimiChannel.unsubscribe(pale);
+        mimiChannel.uploadVideo("SilkSong is the best game ever made");
     }
 }
--- src/main/java/org/example/subject/Channel.java
+++ src/main/java/org/example/subject/Channel.java
@@ -1,34 +1,58 @@
 package org.example.subject;
 
 import org.example.observer.Observer;
+import org.example.observer.User;
 
-public class Channel implements Subject{
+import java.util.HashSet;
+import java.util.Set;
+import java.time.*;
+
+public class Channel implements Subject {
+    // add attributes so that the user can create more than one channel
     private final String name;
+    private final User owner;
 
-    public Channel(String name) {
+    // Subscribers is set to insure that the user on subscribe once
+    private final Set<Observer> subscribers = new HashSet<>();
+
+    public Channel(User owner, String name) {
         this.name = name;
+        this.owner = owner;
     }
 
-    public void uploadVideo(String title){
-        // TODO: Notify all subscribers about new video
+    public void uploadVideo(String title) {
+        LocalDate date = LocalDate.now();
+        System.out.println("[UPLOAD] from " + owner.getUsername() + " to:");
+        String message = name + " uploaded \"" + title + "\" (" + date + ")";
+        notifyObservers(message);
     }
 
     @Override
     public void subscribe(Observer observer) {
-        // TODO: Add subscriber
+        if (!subscribers.add(observer)) {
+            System.out.println("[INFO] \"" + owner.getUsername() + "\" is already subscribed to " + name);
+        }
     }
 
     @Override
     public void unsubscribe(Observer observer) {
-        // TODO: Remove subscriber
+        if (!subscribers.remove(observer)) {
+            System.out.println("[INFO] \"" + owner.getUsername() + "\" is not subscribed to " + name +"\"");
+        }
     }
 
     @Override
     public void notifyObservers(String message) {
-        // TODO: Notify subscribers
+        for (Observer user : subscribers) {
+            user.update(message);
+        }
     }
 
     public String getName() {
         return name;
+    }
+
+    public User getOwner() {
+        return owner;
     }
 }
